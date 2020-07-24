0c3e2676e61bb1ff6692318147a66fd3935cbbed
Gheorghe Pinzaru <feross95@gmail.com>
2020-07-24 10:51:02 +0300

    Contact notifications

--- src/status_im/ethereum/json_rpc.cljs
+++ src/status_im/ethereum/json_rpc.cljs
@@ -144,6 +144,8 @@
    "wakuext_acceptRequestTransaction" {}
    "wakuext_registerForPushNotifications" {}
    "wakuext_unregisterFromPushNotifications" {}
+   "wakuext_enablePushNotificationsFromContactsOnly" {}
+   "wakuext_disablePushNotificationsFromContactsOnly" {}
    "status_chats" {}
    "wallet_getTransfers" {}
    "wallet_getTokensBalances" {}
